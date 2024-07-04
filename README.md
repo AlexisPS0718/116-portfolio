998c80d470df634d6302af58a037c61016fee0ff
Alexis Peñuñuri <alxpesa@gmail.com>
2024-07-03 21:00:33 -0700

    Created responsive header

---
+++ README.md
@@ -0,0 +1,3 @@
+# _FSDI 116: Software Development Methodologies Fundamentals_
+
+Portfolio project including projects, experience and contact information.
---
+++ js/index.js
@@ -0,0 +1,4 @@
+function displayMenu() {
+  document.getElementById("nav-items").style.display = "flex"
+  document.getElementById("ham-menu").style.display = "none"
+}
